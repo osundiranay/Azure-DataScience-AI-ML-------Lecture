fb480e3fb0c9a233bd2b4a5cee4cde71bd7e60af
Nick Nguyen Dai Truong Thanh <36881258+thanhdtnguyen@users.noreply.github.com>
2021-03-13 10:06:06 +0900

    Create measures_of_variance.py

---
+++ data_science/measures_of_variance.py
@@ -0,0 +1,47 @@
+for col_name in ['Grade','StudyHours']:
+    col = df_students[col_name]
+    rng = col.max() - col.min()
+    var = col.var()
+    std = col.std()
+    print('\n{}:\n - Range: {:.2f}\n - Variance: {:.2f}\n - Std.Dev: {:.2f}'.format(col_name, rng, var, std))
+    
+    
+import scipy.stats as stats
+
+# Get the Grade column
+col = df_students['Grade']
+
+# get the density
+density = stats.gaussian_kde(col)
+
+# Plot the density
+col.plot.density()
+
+# Get the mean and standard deviation
+s = col.std()
+m = col.mean()
+
+# Annotate 1 stdev
+x1 = [m-s, m+s]
+y1 = density(x1)
+plt.plot(x1,y1, color='magenta')
+plt.annotate('1 std (68.26%)', (x1[1],y1[1]))
+
+# Annotate 2 stdevs
+x2 = [m-(s*2), m+(s*2)]
+y2 = density(x2)
+plt.plot(x2,y2, color='green')
+plt.annotate('2 std (95.45%)', (x2[1],y2[1]))
+
+# Annotate 3 stdevs
+x3 = [m-(s*3), m+(s*3)]
+y3 = density(x3)
+plt.plot(x3,y3, color='orange')
+plt.annotate('3 std (99.73%)', (x3[1],y3[1]))
+
+# Show the location of the mean
+plt.axvline(col.mean(), color='cyan', linestyle='dashed', linewidth=1)
+
+plt.axis('off')
+
+plt.show()
