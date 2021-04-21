005a286910d1824ea357c1c4032cd253b8bccc96
Nick Nguyen Dai Truong Thanh <36881258+thanhdtnguyen@users.noreply.github.com>
2021-04-21 23:42:48 +0900

    Create azureml_training_script.py

---
+++ azure_machine_learning/azureml_training_script.py
@@ -0,0 +1,54 @@
+# train a script
+
+from azureml.core import Run
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Get the experiment run context
+run = Run.get_context()
+
+# Prepare the dataset
+diabetes = pd.read_csv('data.csv')
+X, y = diabetes[['Feature1','Feature2','Feature3']].values, diabetes['Label'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+
+# Train a logistic regression model
+reg = 0.1
+model = LogisticRegression(C=1/reg, solver="liblinear").fit(X_train, y_train)
+
+# calculate accuracy
+y_hat = model.predict(X_test)
+acc = np.average(y_hat == y_test)
+run.log('Accuracy', np.float(acc))
+
+# Save the trained model
+os.makedirs('outputs', exist_ok=True)
+joblib.dump(value=model, filename='outputs/model.pkl')
+
+run.complete()
+
+# run a script as an experiment
+
+from azureml.core import Experiment, ScriptRunConfig, Environment
+from azureml.core.conda_dependencies import CondaDependencies
+
+# Create a Python environment for the experiment
+sklearn_env = Environment("sklearn-env")
+
+# Ensure the required packages are installed
+packages = CondaDependencies.create(conda_packages=['scikit-learn','pip'],
+                                    pip_packages=['azureml-defaults'])
+sklearn_env.python.conda_dependencies = packages
+
+# Create a script config
+script_config = ScriptRunConfig(source_directory='training_folder',
+                                script='training.py',
+                                environment=sklearn_env) 
+
+# Submit the experiment
+experiment = Experiment(workspace=ws, name='training-experiment')
+run = experiment.submit(config=script_config)
+run.wait_for_completion()
